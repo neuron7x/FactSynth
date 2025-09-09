d8de45fc6aec92e1910f69dc8b677c838648504b
Yaroslav Vasylenko <stapkabenderka@ukr.net>
2025-09-09 16:51:52 +0300

    refactor: consolidate ISR sim imports

--- src/factsynth_ultimate/isr/__init__.py
+++ src/factsynth_ultimate/isr/__init__.py
@@ -1,17 +1,9 @@
 from .sim import (
-    ISRParams as ISRParams,
-)
-from .sim import (
-    dominant_freq as dominant_freq,
-)
-from .sim import (
-    estimate_fs as estimate_fs,
-)
-from .sim import (
-    gamma_spectrum as gamma_spectrum,
-)
-from .sim import (
-    simulate_isr as simulate_isr,
+    ISRParams,
+    dominant_freq,
+    estimate_fs,
+    gamma_spectrum,
+    simulate_isr,
 )
 
 __all__ = [
