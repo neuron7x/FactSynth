5ba8584b0133a5415bef14a39b6946756597219b
Yaroslav Vasylenko <stapkabenderka@ukr.net>
2025-09-13 10:58:27 +0300

    Use schemathesis.openapi.from_dict

--- tests/test_openapi_contract.py
+++ tests/test_openapi_contract.py
@@ -11,8 +11,10 @@ OPENAPI_SPEC = {
     "paths": {"/v1/score": {"post": {"responses": {"200": {"description": "OK"}}}}},
 }
 
-schema = schemathesis.from_dict(OPENAPI_SPEC, base_url="http://test")
+schema = schemathesis.openapi.from_dict(OPENAPI_SPEC)
 schema.validate()
+
+pytestmark = pytest.mark.httpx_mock(assert_all_responses_were_requested=False)
 
 
 def test_openapi_schema_has_operations() -> None:
